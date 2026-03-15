b0e1c5df010b4be6afef8b162b1503e4e0d664c3
rtyty0615 <174629541+rtyty0615@users.noreply.github.com>
2026-03-15 11:33:42 +0800

    Update

--- webpack.common.js
+++ webpack.common.js
@@ -25,6 +25,11 @@ export default {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
         ],
+        
     },
 };
