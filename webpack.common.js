d606bb67a1156ea70f472a2d07135dee2ca0e773
rtyty0615 <174629541+rtyty0615@users.noreply.github.com>
2026-03-16 10:22:40 +0800

    Link template.html to common.js

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ export default {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Production',
+            template: './src/template.html',
         }),
     ],
     output: {
